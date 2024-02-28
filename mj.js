e9dd482df4250dc7281d10d8125171b8c7efacdb
SwaraliBachhav16 <161601562+SwaraliBachhav16@users.noreply.github.com>
2024-02-28 19:25:46 +0530

    Add files via upload

---
+++ mj.js
@@ -0,0 +1,78 @@
+const product = [
+    {
+        id: 0,
+        image:'mj1.jpg',
+        title: 'jeans-1',
+        price:500.00,
+    },
+    {
+        id: 1,
+        image: 'mj2.jpg',
+        title: 'jeans-2',
+        price: 800,
+    },
+    {
+        id: 2,
+        image: 'mj3.jpg',
+        title: 'jeans-3',
+        price: 700,
+    },
+  ];
+  const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+  document.getElementById('root').innerHTML = categories.map((item)=>
+  {
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>&#8377 ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+  }).join('')
+  
+  var cart =[];
+  
+  function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+  }
+  function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+  }
+  
+  function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "&#8377"+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "&#8377 "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>&#8377${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+  
+    
+  }
