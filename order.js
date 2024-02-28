b11ec0f9163f1c2fde5791023985a4a54aa1da6c
SwaraliBachhav16 <161601562+SwaraliBachhav16@users.noreply.github.com>
2024-02-28 19:24:45 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const order=document.getElementById('order')
+    order.addEventListener('click', function () {
+        // Redirect to the signup page
+        window.location.href = 'order.html';
+    });
+});
